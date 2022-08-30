6febb9ce4bf53226521d733473b6f95dc8c3f65d
David Moore <david_e_moore@hotmail.com>
2022-08-30 15:44:06 -0700

    SuperNinja Assignment Implement Inheritance+Super

--- MERN/javascript/ninja.js
+++ MERN/javascript/ninja.js
@@ -25,3 +25,21 @@ const ninja1 = new Ninja("Hyabusa", 100);
 ninja1.sayName();
 console.log(ninja1.showStats());
 console.log(ninja1.drinkSake())
+
+class Sensei extends Ninja {
+    constructor(name, health = 200, speed = 10, strength = 10, wisdom = 10){
+        super(name, health, speed, strength);
+        this.wisdom = wisdom;
+    }
+
+    speakWisdom(){
+        super.drinkSake();
+        console.log(this.health)
+        console.log('What one programmer can do in one month, two programmers can do in two months!');
+    }
+
+}
+
+const sensei1 = new Sensei('Master Splinter');
+sensei1.speakWisdom();
+console.log(sensei1.showStats());
