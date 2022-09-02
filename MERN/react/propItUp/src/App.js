eebb8514c30b9b37c322313ccf974abb2103049d
David Moore <david_e_moore@hotmail.com>
2022-09-01 19:24:34 -0700

    Putting It Together - State & Event Use

--- MERN/react/propItUp/src/App.js
+++ MERN/react/propItUp/src/App.js
@@ -2,16 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import PersonCard from './components/PersonCard';
 
+let people = [
+  { "firstName": "Jane", "lastName": "Doe", "age": 45, "hairColor": "Black"},
+  { "firstName": "John", "lastName": "Smith", "age": 88, "hairColor": "Brown"},
+  { "firstName": "Millard", "lastName": "Fillmore", "age": 50, "hairColor": "Brown"},
+  { "firstName": "Maria", "lastName": "Smith", "age": 62, "hairColor": "Brown"},
+];
+
 function App() {
   return (
     <div className="App">
-      <PersonCard firstName={"Jane"} lastName={"Doe"} age={"45"} hairColor={"Black"} />
+      {/* <PersonCard firstName={"Jane"} lastName={"Doe"} age={"45"} hairColor={"Black"} />
       <PersonCard firstName={"John"} lastName={"Smith"} age={"88"} hairColor={"Brown"} />
       <PersonCard firstName={"Millard"} lastName={"Fillmore"} age={"50"} hairColor={"Brown"} />
-      <PersonCard firstName={"Maria"} lastName={"Smith"} age={"62"} hairColor={"Brown"} />
+      <PersonCard firstName={"Maria"} lastName={"Smith"} age={"62"} hairColor={"Brown"} /> */}
+
+      { people.map(person => {
+        return <PersonCard firstName={person.firstName} lastName={person.lastName} age={person.age} hairColor={person.hairColor} />
+      })}
     </div>
   );
 }
 
 export default App;
-  
--- MERN/react/propItUp/src/components/PersonCard.js
+++ MERN/react/propItUp/src/components/PersonCard.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
 
 class PersonCard extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            age: this.props.age
+        }
+    }
+    addToAge = () => {
+        this.setState((state) => {
+            console.log(this.state.age)
+            return {age: state.age + 1}
+        })
+    }
+
     render() {
         const { firstName, lastName, age, hairColor } = this.props;
         return (
             <div className='Flex'>
             <div className="Align">
                 <h1>{ lastName }, {firstName}</h1>
-                <p>Age: {age}</p>
+                <p>Age: {this.state.age}</p>
                 <p>Hair Color: {hairColor}</p>
+                <button onClick={this.addToAge}>Birthday Button for {firstName} {lastName}</button>
             </div>
             </div>
         );
